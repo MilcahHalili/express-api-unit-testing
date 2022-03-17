81c94179f903d2ac330b91fa5ca3aa2f2ad499fa
Milcah <code@milcah.dev>
2022-03-16 21:48:38 -0700

    installs morgan logger

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const routes = require('./routes')
+const logger = require('morgan')
 
 const PORT = process.env.PORT || 8000
 
 const app = express()
 
 app.use(express.json())
+app.use(logger('dev'))
 
 app.use('/api', routes)
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+
+module.exports = app
