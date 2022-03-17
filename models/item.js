ac9bb9391d3d0a2db7ee6833d9974facdc87fbfe
Milcah <code@milcah.dev>
2022-03-16 21:31:54 -0700

    creates create user function

--- models/item.js
+++ models/item.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Item.belongsTo(User, {
+      Item.belongsTo(models.User, {
         foreignKey: 'userId',
         onDelete: 'CASCADE'
       })
