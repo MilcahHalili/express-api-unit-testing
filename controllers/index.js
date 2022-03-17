8608c8b8557ffab2fc14ba911b32d5daca3a4f31
Milcah <code@milcah.dev>
2022-03-16 21:37:09 -0700

    creates get all users controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const { User } = require('../models')
+const { Item } = require('../models')
 
 const createUser = async (req, res) => {
   try {
@@ -11,6 +12,22 @@ const createUser = async (req, res) => {
   }
 }
 
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll({
+      include: [
+        {
+          model: Item
+        }
+      ]
+    })
+    return res.status(200).json({ users })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 module.exports = {
-  createUser
+  createUser,
+  getAllUsers
 }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const router = Router()
 
 router.get('/', (req, res) => res.send('This is root!'))
 router.post('/users', controllers.createUser)
+router.get('/users', controllers.getAllUsers)
 
 module.exports = router
