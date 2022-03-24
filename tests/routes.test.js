65e7644f10b281e1759e312f940d2c988e05cce1
Milcah <code@milcah.dev>
2022-03-23 21:37:11 -0700

    writes passing create user test

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -2,11 +2,25 @@ const request = require('supertest')
 const app = require('../server.js')
 
 describe('User API', () => {
+
+  it('should create a user', async () => {
+    const res = await request(app)
+      .post('/api/users')
+      .send({
+        firstName: 'Bob',
+        lastName: 'Hope',
+        email: 'bob@hope.com',
+        password: '12345678'
+      })
+      expect(res.statusCode).toEqual(201) 
+      expect(res.body).toHaveProperty('user')
+  }),
+
   it('should show all users', async () => {
     const res = await request(app).get('/api/users')
     expect(res.statusCode).toEqual(200)
     expect(res.body).toHaveProperty('users')
-  })
+  }),
 
   it('should show a user', async () => {
     const res = await request(app).get('/api/users/3')
